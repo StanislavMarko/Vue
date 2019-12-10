6d3d36bce5a0710c53f2c119fd5bf25f0406f3b6
StanislavMarko <stas.m@intobi.com>
2019-12-10 15:17:13 +0200

    lesson 10 Vue.js

--- main.js
+++ main.js
@@ -26,36 +26,55 @@
 // new Vue({
 //     el:'#root'
 // });
-Vue.component('message', {
-    props:['title', 'body'],
+// Vue.component('message', {
+//     props:['title', 'body'],
+//
+//     data() {
+//         return{
+//             isVisible: true
+//         };
+//     },
+//
+//     template:`
+//     <article class="message" v-show="isVisible">
+//         <div class="message-header">
+//             {{ title }}
+//
+//             <button type="button" @click="isVisible = false">x</button>
+//         </div>
+//         <div class="message-body">
+//             {{ body }}
+//         </div>
+//     </article>
+//     `,
+//
+//     // methods: {
+//     //     hideModal() {
+//     //         this.isVisible = false;
+//     //
+//     //     }
+//     // }
+// });
 
-    data() {
-        return{
-            isVisible: true
-        };
-    },
-
+Vue.component('modal',{
     template:`
-    <article class="message" v-show="isVisible">
-        <div class="message-header">
-            {{ title }}
-            
-            <button type="button" @click="hideModal">x</button>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+               <slot></slot>
+            </div>
         </div>
-        <div class="message-body">
-            {{ body }}
-        </div>
-    </article>
-    `,
-
-    methods: {
-        hideModal() {
-            this.isVisible = false;
-
-        }
-    }
+        <button class="modal-close is-large" @click="$emit('close')"></button>
+    </div>
+    `
 });
 
 new Vue({
-    el:'#root'
+    el:'#root',
+
+    data:{
+        showModal:false
+
+    }
 });
