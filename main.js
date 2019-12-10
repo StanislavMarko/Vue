d24c21f4527a1ac988b3d2b566d4d8bcf7dde0dc
StanislavMarko <stas.m@intobi.com>
2019-12-10 14:18:18 +0200

    vue.js files

---
+++ main.js
@@ -0,0 +1,28 @@
+//
+// Vue.component('task-list' , {
+//     template: `<div><task v-for="task in tasks">{{ task.task }}</task></div>`,
+//
+//     data() {
+//         return {
+//             tasks:[
+//                 {task:'go to the store', complete: true},
+//                 {task:'go to the school', complete: false},
+//                 {task:'go to the farm', complete: true},
+//                 {task:'go to the work', complete: false}
+//             ]
+//
+//         };
+//     }
+// });
+// Vue.component('task' ,{
+//     template: '<li><slot></slot></li>'
+//
+//     // data: {
+//     //
+//     // }
+// });
+//
+//
+// new Vue({
+//     el:'#root'
+// });
